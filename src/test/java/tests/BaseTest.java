67c2a7ec7ae7aa12d1f1fb32877619ba6275c4f4
aleksandrakoneva <akoneva@alfabank.ru>
2024-04-21 21:57:43 +0300

    доработка по докеру

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.pages.CartPage;
 import org.example.pages.DigmaPage;
 import org.example.pages.MainPage;
@@ -7,6 +8,7 @@ import org.example.pages.SearchPage;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -15,25 +17,30 @@ import java.net.URL;
 
 public class BaseTest {
 
-//    protected WebDriver driver;
+    //protected WebDriver driver; //todo для локального запуска раскомментировать
     protected MainPage mainPage;
     protected DigmaPage digmaPage;
     protected CartPage cartPage;
     protected SearchPage searchPage;
 
-    private RemoteWebDriver driver;
+    private RemoteWebDriver driver; //todo для удаленного запуска раскомментировать
 
     @Before
     public void setUp() {
+        // todo ОТСЮДА для удаленного запуска
         ChromeOptions options = new ChromeOptions();
         options.setCapability("browserName", "chrome");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         try {
-            driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+            driver = new RemoteWebDriver(new URL("http://selenium-hub:4444"), options);
         } catch (MalformedURLException e) {
-            e.printStackTrace();
         }
+        // todo ДОСЮДА для удаленного запуска
+
+        //WebDriverManager.chromedriver().setup(); //todo для локального запуска раскомментировать
+        //driver = new ChromeDriver(); //todo для локального запуска раскомментировать
+
 
         //страницы
         this.mainPage = new MainPage(driver);
